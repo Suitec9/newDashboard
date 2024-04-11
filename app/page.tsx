9529c547cb3826af340cfad9ee252119f8a9c64b
Suitec9 <Boommaigacalgang@duck.com>
2024-04-11 10:25:32 +0200

    🧐️💯️

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,7 @@ import Layout from './ui/dashboard/layout';
 
 export default function Page() {
   return (
+    
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
          <AcmeLogo /> 
@@ -29,7 +30,6 @@ export default function Page() {
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
-            <Layout children={undefined}/>
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
@@ -50,5 +50,6 @@ export default function Page() {
         </div>
       </div>
     </main>
+    
   );
 }
