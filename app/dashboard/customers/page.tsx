9d7c37279f58957646ff30f85c2af5e559d43f87
Suitec9 <Boommaigacalgang@duck.com>
2024-06-11 15:31:28 +0200

    Intial deployemt🌐️🫥️💻️

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,4 +1,8 @@
-
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Customers 🤑️ | Acme Dashboard',
+};
 export default function Page()  {
   return (
     <p>Customers🤑️z Page</p>
